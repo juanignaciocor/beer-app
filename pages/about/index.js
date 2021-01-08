4068222c1b608b0560530ce663a35b42c0c22d65
jodch <56612511+jodch@users.noreply.github.com>
2021-01-07 21:58:57 -0300

    singleview

--- pages/about/index.js
+++ pages/about/index.js
@@ -4,10 +4,11 @@ import Header from "../../components/Header/header"
 import Footer from "../../components/Footer/footer"
 
 const About = () => {
-  return <div className={styles.aboutcontainer}>
+  return <div className='pagecontainer'>
 <Header/>
-    
-    about
+    <p className={styles.textabout}>
+      Somos un club de birra para aprender de birra y tomar con tus amigos jugando al csgo.
+    </p>
     <Footer/>
     </div>;
 };
